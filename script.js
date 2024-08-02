a1a26708f1f34e1faff3c05538e48a3875aa7e3c
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-08-02 16:59:59 -0300

    Alterações complexas

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ var msgGlobal;
 function conferirMensagemWhatsApp() {
   var nome = document.getElementById("nome").value;
   var mensagem = document.getElementById("mensagem").value;
+  var confDiv = document.getElementById("rightDiv");
 
   nomeGlobal = nome;
   msgGlobal = mensagem;
 
+  confDiv.style.display = "flex"
   document.getElementById("confNome").textContent = nome;
   document.getElementById("confMsg").textContent = mensagem;
 }
 
 function enviar() {
-  var numeroTelefone = "5541988314797";
+  var numeroTelefone = "5541988314597";
 
   var link =
     "https://wa.me/" +
